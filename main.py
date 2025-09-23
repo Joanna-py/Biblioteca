21f6658f695f561847d081a324c849246909fd12
Joanna Santos <joanna.d.souza@aluno.senai.br>
2025-09-23 12:45:23 -0300

    "Etapa 3 - Função Listagem de livros"

--- main.py
+++ main.py
@@ -45,3 +45,23 @@ autor = input(f"Quem é o autor do livro? -| {titulo}: ")
 ano = int(input(f"De que ano é o livro? -| {titulo}: "))
 
 cadastrar_livro(titulo, autor, ano)
+
+
+#Etapa 3
+
+def listar_livros():
+    try:
+        conexao = sqlite3.connect("biblioteca.db")
+        cursor = conexao.cursor()
+
+        cursor.execute("SELECT * FROM livros")
+        print("\n=== LISTA DE LIVROS ===")
+        for linha in cursor.fetchall():
+            print(f"ID {linha[0]} | TÍTULO {linha[1]} | AUTOR {linha[2]} | ANO {linha[3]} | DISPONÍVEL {linha[4]}")
+
+    except sqlite3.Error as erro:
+        print(f"Erro ao listar livros: {erro}")
+    finally:
+        conexao.close()
+
+listar_livros()
